269703baa2c46dbf367f740fc80ecdb72a5c22d4
sebastiangallego0317 <samuelmontes1105@gmail.com>
2023-12-29 01:17:50 -0500

    feat: finish splash part

--- src/components/pages/Splash.tsx
+++ src/components/pages/Splash.tsx
@@ -1,3 +1,19 @@
-export default function Splash(){
-    return <>Hello World</>
+import { useNavigate } from 'react-router-dom'
+import { COMPANY_NAME } from '../../lib/constants/constants'
+import Logo from '../atoms/Logo'
+import { useEffect } from 'react'
+
+export default function Splash() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/home')
+    }, 2000)
+  }, [])
+  return (
+    <main className="page-wrapper splash-page-wrapper">
+      <Logo width="80" height="80" />
+      <h1 className="splash-title">{COMPANY_NAME}</h1>
+    </main>
+  )
 }
