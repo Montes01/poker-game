775d2da9bfc013a14fcce4d6220e9d688d64fa04
Montes15 <samuelmontes1105@gmail.com>
2024-02-22 11:21:22 -0500

    feat : Removed Mock Folder

--- src/lib/mock/room.ts
+++
@@ -1,3 +0,0 @@
-export const activeRooms = new Promise((resolve) => {
-  resolve(['1', 'ubafkajbfa', '3'])
-})
---
+++ src/tests/enum.test.ts
@@ -0,0 +1,15 @@
+import {playerType, inputTypes, ioEvents, } from "../lib/constants/declarations"
+
+describe("enum", () => {
+  it("should match the snapshot", () => {
+    expect(playerType).toMatchSnapshot()
+    expect(inputTypes).toMatchSnapshot()
+    expect(ioEvents).toMatchSnapshot()
+  })
+})
+
+const inputTypesT = {
+
+
+
+}
