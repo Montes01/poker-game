2ba0435d405ce83c6605af7c4eaf54f4973d413f
Montes01 <samuelmontes1105@gmail.com>
2024-04-11 14:11:49 -0500

    feat: All Atoms tested

--- src/system-design/atoms/Table.tsx
+++ src/system-design/atoms/Table.tsx
@@ -3,5 +3,5 @@ interface Props {
   children?: React.ReactNode
 }
 export default function Table({ children }: Props) {
-  return <div className="game-table">{children}</div>
+  return <div className="game-table" role="game-table">{children}</div>
 }
