faed0b7a494ce830b2f7901679fa9178758f3f9c
Montes01 <samuelmontes1105@gmail.com>
2024-04-24 17:39:42 -0500

    feat: aws configured pending: make the ligin component

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "pokergame69a1c1d6": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import { Route, Routes } from "react-router-dom"
 import Splash from "./pages/Splash"
 import Home from "./pages/Home."
 import Room from "./pages/Room"
-
+import { Amplify } from "aws-amplify"
+import config from "./aws-exports.js" 
+import { useEffect, useState } from "react"
+Amplify.configure(config)
 export default function App() {
+  const [isLogged, setIsLogged] = useState(false)
+  useEffect(() => {
+    
+  }, [])
   return (
     <>
       <Routes>
